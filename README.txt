3401747acc845364066a9b0532a280878811d60e
SamuelLBau <samuellbauza@gmail.com>
2016-03-05 16:16:18 -0800

    Update .gitignore

---
+++ README.txt
@@ -0,0 +1,9 @@
+This is the GUI for Harpy Eagles, it is programmed with the following:
+
+
+OS: Windows 10
+Version: Python 2.7
+
+Library dependancies:
+-Pillow
+-numpy
---
+++ libraries.py
@@ -0,0 +1,6 @@
+# A short program that displays what libraries are installed
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+     for i in installed_packages])
+print(installed_packages_list)
