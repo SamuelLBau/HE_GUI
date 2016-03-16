99eebb8a9e44abb93e405e25942500ebefbfb65b
SamuelLBau <samuellbauza@gmail.com>
2016-03-15 17:59:13 -0700

    More installer updates

--- HE_GUI.bat
+++ HE_GUI.bat
@@ -1,5 +1,14 @@
-PATH=%PATH%;C:/PYTHON27
+@echo off
+rem set to this directory
+set PROGRAM_DIR=%~dp0
 
+set
+
+set PROGRAM_DIR=%PROGRAM_DIR:\\=\%
+echo. & echo PROGRAM_DIR home is %PROGRAM_DIR% & echo.
+
+set PATH = %PATH%;C:/PYTHON27
 
 python ./gui_layer/gui.py
 
+@echo on
